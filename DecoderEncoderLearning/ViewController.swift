bf0e383561701da6da409ae9a0361d25fafca1dd
Akshansh Thakur <aksh500@gmail.com>
2018-08-15 12:15:58 +0530

    Initial Commit

---
+++ DecoderEncoderLearning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DecoderEncoderLearning
+//
+//  Created by mac on 5/24/1397 AP.
+//  Copyright © 1397 AkshiOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
